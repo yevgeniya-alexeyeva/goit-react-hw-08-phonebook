711e3b23aad7475b6c59a03cf620590b663d8503
Yevgeniya Alexeyeva <y.alekseyeva@gmail.com>
2021-06-03 16:24:51 +0600

    Updates

---
+++ static/js/HomeView.36542ebe.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[2],{207:function(t,o,e){"use strict";e.r(o),e.d(o,"default",(function(){return c}));var n=e(2),c=function(){return Object(n.jsx)("h1",{children:"Home"})}}}]);
+//# sourceMappingURL=HomeView.36542ebe.chunk.js.map
